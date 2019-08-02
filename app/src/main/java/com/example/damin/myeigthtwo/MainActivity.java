eb6409550ccc6d5a808e9abf8a73b5547179389b
lwh123456 <2365806605@qq.com>
2019-08-02 10:58:19 +0800

    //第二行代码  吐司

--- app/src/main/java/com/example/damin/myeigthtwo/MainActivity.java
+++ app/src/main/java/com/example/damin/myeigthtwo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.damin.myeigthtwo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate: 我是李武辉");
+        Toast.makeText(this, "李武辉::::::", Toast.LENGTH_SHORT).show();
     }
 
     private static final String TAG = "MainActivity";
